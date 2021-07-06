2928d7827a01d7b980e00db889a97b8e9083363b
lulungsatrioprayuda <alungsatrio12@gmail.com>
2021-07-06 22:24:55 +0700

    Install
    Install and create new page

---
+++ src/views/Foods.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Navbar />
+    <h2>List foods</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "Foods",
+  components: {
+    Navbar
+  }
+};
+</script>
+
+<style>
+</style>
