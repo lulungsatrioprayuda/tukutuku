c6b63f91ae64394d0204f2e26e3f366ea010ad5f
lulungsatrioprayuda <alungsatrio12@gmail.com>
2021-07-07 19:09:44 +0700

    Update Hero.vue
    make mobile ui and dekstop ui mode

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,31 +1,54 @@
 <template>
-  <div class="row">
-    <div class="col">
-      <div class="d-flex h-100">
-        <div class="justify-content-center align-self-center">
-          <h2>
-            <strong>Makanan yang lezat,</strong>
-            <br />ada di Gadetmu!
-          </h2>
-          <p>Tunggu apalagi? ambil hp Anda, lalu pesan makanan lezatnya</p>
-          <button class="btn btn-success">
-            <b-icon-arrow-right />Pesan
-          </button>
+  <div class="hero">
+    <!-- Dekstop -->
+    <div class="row mt-4">
+      <div class="col-md-6">
+        <div class="d-flex h-100">
+          <div class="justify-content-center align-self-center">
+            <h2>
+              <strong>Makanan yang lezat,</strong>
+              <br />ada di Gadetmu!
+            </h2>
+            <p>Tunggu apalagi? ambil hp Anda, lalu pesan makanan lezatnya</p>
+            <button class="btn btn-success">
+              <b-icon-arrow-right />&nbsp;Pesan
+            </button>
+          </div>
         </div>
+      </div>
+      <div class="col-md-6">
+        <img src="../assets/images/hero.png" width="100%" />
       </div>
     </div>
 
-    <div class="col">
-      <img src="../assets/images/hero.png" width="100%" />
+    <!-- Mobile -->
+    <div class="row mt-4">
+      <div class="col-md-6">
+        <div class="d-flex h-100">
+          <div class="justify-content-center align-self-center">
+            <h2>
+              <strong>Makanan yang lezat,</strong>
+              <br />ada di Gadetmu!
+            </h2>
+            <p>Tunggu apalagi? ambil hp Anda, lalu pesan makanan lezatnya</p>
+            <button class="btn btn-success">
+              <b-icon-arrow-right />&nbsp;Pesan
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-6">
+        <img src="../assets/images/hero.png" width="100%" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Hero"
+  name: "Hero",
 };
 </script>
 
-<style>
-</style>
+<style></style>
