a00c72e3fd3da6c60006bbd6c92a216702efc115
lulungsatrioprayuda <alungsatrio12@gmail.com>
2021-07-08 00:22:16 +0700

    card
    at 25 and 28

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <Navbar />
     <div class="container">
       <Hero />
+
       <div class="row mt-4">
         <div class="col">
           <h2>
@@ -12,14 +13,14 @@
         </div>
         <div class="col">
           <router-link to="/foods" class="btn btn-success float-right">
-            <b-icon-eye></b-icon-eye>&nbsp;Lihat Semua
+            <b-icon-eye></b-icon-eye>Lihat Semua
           </router-link>
         </div>
       </div>
 
-      <div class="row mb-3">
-        <div class="col-md-3 mt-4" v-for="product in products">
-          <CardProduct />
+      <div class="row mb-4">
+        <div class="col-md-4 mt-4" v-for="product in products" :key="product.id">
+          <CardProduct :product="product" />
         </div>
       </div>
     </div>
@@ -45,22 +46,16 @@ export default {
       products: [],
     };
   },
-  method: {
-    setProduct(data) {
+  methods: {
+    setProducts(data) {
       this.products = data;
     },
   },
   mounted() {
     axios
       .get("http://localhost:3000/best-products")
-      .then(function(response) {
-        // handle success
-        console.log(response.data);
-      })
-      .catch(function(error) {
-        // handle error
-        console.log("Gagal! : ", error);
-      });
+      .then((response) => this.setProducts(response.data))
+      .catch((error) => console.log(error));
   },
 };
 </script>
