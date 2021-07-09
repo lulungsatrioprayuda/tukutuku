65ab92554d541d98fcfe26abf9ebda61a1eff264
lulungsatrioprayuda <alungsatrio12@gmail.com>
2021-07-10 00:01:45 +0700

    update ui
    change picture

--- src/components/CardProduct.vue
+++ src/components/CardProduct.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="card shadow card-product">
     <img
-      :src="require(`../assets/images/${product.gambar}`)"
       class="card-img-top"
+      :src="require(`../assets/images/${product.gambar}`)"
       alt="..."
     />
     <div class="card-body">
       <h5 class="card-title">{{ product.nama }}</h5>
-      <p class="card-text">Harga : Rp. {{ product.harga }}</p>
+      <p class="card-text">Harga : Rp.{{ product.harga }}</p>
       <router-link class="btn btn-success" :to="'/foods/' + product.id">
-        <b-icon-cart></b-icon-cart>Pesan
+        <b-icon-cart></b-icon-cart> Pesan
       </router-link>
     </div>
   </div>
