08898a05ffb6e3372de4673a4e50948f99e3fdaf
lulungsatrioprayuda <alungsatrio12@gmail.com>
2021-07-24 18:30:18 +0700

    Update FoodDetail.vue
    add api for call data

--- src/views/FoodDetail.vue
+++ src/views/FoodDetail.vue
@@ -2,15 +2,32 @@
   <div class="food-detail">
     <Navbar />
     <div class="container">
+      <!-- breadcrumb -->
       <div class="row mt-5">
         <div class="col">
           <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-              <li class="breadcrumb-item" ><router-link to="/" class="text-dark" >Home</router-link></li>
-              <li class="breadcrumb-item" ><router-link to="/foods" class="text-dark" >Foods</router-link></li>
-              <li class="breadcrumb-item active" >Food Order</li>
+              <li class="breadcrumb-item">
+                <router-link to="/" class="text-dark">Home</router-link>
+              </li>
+              <li class="breadcrumb-item">
+                <router-link to="/foods" class="text-dark">Foods</router-link>
+              </li>
+              <li class="breadcrumb-item active">Food Order</li>
             </ol>
           </nav>
+        </div>
+      </div>
+
+      <!-- foodDetail -->
+      <div class="row">
+        <div class="col-md-6">
+          <!-- <img :src="require(`../assets/image/${product.gambar}`)" alt=""> -->
+        </div>
+        <div class="col-md-6">
+          <h2>
+            <strong>{{ product.nama }}</strong>
+          </h2>
         </div>
       </div>
     </div>
@@ -19,11 +36,28 @@
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import axios from "axios";
 
 export default {
   name: "FoodDetail",
   components: {
     Navbar,
+  },
+  data() {
+    return {
+      product: {}
+    }
+  },
+  methods: {
+    setProduct(data) {
+      this.product = data;
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/product/" + this.$route.params.id)
+      .then((response) => this.setProduct(response.data))
+      .catch((error) => console.log(error));
   },
 };
 </script>
