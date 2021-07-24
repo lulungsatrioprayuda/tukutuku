0a6280cc25521dbb99f0a1ca864b349034b08e92
lulungsatrioprayuda <alungsatrio12@gmail.com>
2021-07-24 17:02:37 +0700

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,17 @@
 <template>
   <footer class="mt-5 mb-5">
-      <hr />
-      <div class="container">
-          <div class="row">
-              <div class="col text-center">
-                  2021 Copyright lulungstore
-              </div>
-          </div>
+    <hr />
+    <div class="container">
+      <div class="row">
+        <div class="col text-center">2021 Copyright lulungstore</div>
       </div>
+    </div>
   </footer>
 </template>
 
 <script>
 export default {
-    name: "Footer"
+  name: "Footer",
 };
 </script>
 
