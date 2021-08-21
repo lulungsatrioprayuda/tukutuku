a885a47ac01f77686c1cd805fbdf6585c8c05e8b
lulungsatrioprayuda <alungsatrio12@gmail.com>
2021-08-21 21:56:54 +0700

    add data function
    progress add data to cart and add some toast response

--- src/main.js
+++ src/main.js
@@ -6,10 +6,13 @@ import {
   BootstrapVueIcons
 } from 'bootstrap-vue'
 
+import VueToast from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-sugar.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/css/main.css'
 
+Vue.use(VueToast)
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 
--- src/views/FoodDetail.vue
+++ src/views/FoodDetail.vue
@@ -22,24 +22,43 @@
       <!-- foodDetail -->
       <div class="row mt-3">
         <div class="col-md-6">
-          <img :src="'../assets/images/'+product.gambar" class="img-fluid shadow" alt="">
+          <img
+            :src="'../assets/images/' + product.gambar"
+            class="img-fluid shadow"
+            alt=""
+          />
         </div>
         <div class="col-md-6">
           <h2>
             <strong>{{ product.nama }}</strong>
           </h2>
-          <hr>
-          <h4>Harga : <strong>Rp. {{product.harga}}</strong></h4>
-          <form class="mt-4">
-              <div class="form-group">
-                  <label for="jumlah_pemesanan">Jumlah Pesan</label>
-                  <input type="number" class="form-control" >
-              </div>
-              <div class="form-group">
-                  <label for="keterangan">Keterangan</label>
-                  <textarea class="form-control" name="" placeholder="Keterangan contoh : Nasi sedikit, Tidak usah lauk"></textarea>
-              </div>
-              <button type="submit" class="btn btn-success"><b-icon-cart></b-icon-cart> Pesan</button>
+          <hr />
+          <h4>
+            Harga : <strong>Rp. {{ product.harga }}</strong>
+          </h4>
+          <!-- fungsi  v-on:submit.prevent adalah untuk mencegah reload pada saat submit pada form-->
+          <form class="mt-4" v-on:submit.prevent>
+            <div class="form-group">
+              <label for="jumlah_pemesanan">Jumlah Pesan</label>
+              <!-- v model bisa di bilang untuk mendaftarkan sebuah inputan kedalam data yang bernama pesanan, untuk jumlah_pemesanan itu adalah nama bebas seperti nama di adalam property "name" pada inputan -->
+              <input
+                type="number"
+                class="form-control"
+                v-model="pesan.jumlah_pemesanan"
+              />
+            </div>
+            <div class="form-group">
+              <label for="keterangan">Keterangan</label>
+              <!-- pesan.keterangan artinya adalah pesan adalah "nama data yang bernamakan pesan" lalu keterangan adalah sebuah inputan yang akan di masukan ke dalam data "pesan"-->
+              <textarea
+                class="form-control"
+                v-model="pesan.keterangan"
+                placeholder="Keterangan contoh : Nasi sedikit, Tidak usah lauk"
+              ></textarea>
+            </div>
+            <button type="submit" class="btn btn-success" @click="pemesanan">
+              <b-icon-cart></b-icon-cart> Pesan
+            </button>
           </form>
         </div>
       </div>
@@ -58,12 +77,27 @@ export default {
   },
   data() {
     return {
-      product: {}
-    }
+      product: {},
+      pesan: {},
+    };
   },
   methods: {
     setProduct(data) {
       this.product = data;
+    },
+    pemesanan() {
+      this.pesan.products = this.product;
+      axios
+        .post("http://localhost:3000/keranjangs", this.pesan)
+        .then(() => {
+          this.$toast.success("Sukses Menambahkan Makanan.", {
+            type: 'success',
+            position: 'top-right',
+            duration: 3000,
+            dismissible: true
+          });
+        })
+        .catch((err) => console.log(err));
     },
   },
   mounted() {
